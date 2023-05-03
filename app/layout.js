814588a2c967ec1d648675c573fee2ecbf7e5374
guilledev <guillermoprogcc@gmail.com>
2023-05-03 00:09:18 -0400

    add layaut folder and created my firts render without errors

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Aleteration R&G',
+  description: 'Somos un servicio profesional de alteraciones de ropa con años de experiencia en el sector. Ofrecemos una amplia variedad de servicios de costura, desde pequeñas reparaciones hasta ajustes complejos en todo tipo de prendas. Nos especializamos en arreglos de ropa para hombres, mujeres y niños. Además, ofrecemos servicios de personalización, como bordados y apliques. Contáctanos para obtener una cotización personalizada y confía en nosotros para que tus prendas queden perfectas.',
+  keywords: 'arreglos de ropa, costura, reparaciones, ajustes, personalización, bordados, apliques, servicios, hombres, mujeres, niños, confianza.'
+}
+ 
+export default function RootLayout({ children }) {
+ return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>My first app with next js</div>;
+}
+
+export default page;

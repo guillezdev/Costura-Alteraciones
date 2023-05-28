f531e3ea4330ef847385ba1a429338bafe122543
guillezdev <guillermoprogcc@gmail.com>
2023-05-27 18:36:40 -0700

    fix errors

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,23 +1,21 @@
-import React from "react";
 
-function Header() {
+export function Header() {
   return (
     <>
       <header className="text-white  bg-gradientedown">
         <div className="container text-center mx-auto py-12 px-4 sm:px-6 lg:px-8">
           <div>
-            <h1 className="text-6xl bg-clip-text text-transparent bg-gradient-to-r from-white to-orange-500 font-bold mb-4">
+            <h1 className="text-6xl font-bold mb-4">
               L&R Aleteration
             </h1>
             <p className="text-xl mb-8">
-              Aquí encontrarás información sobre los servicios de costura que
-              ofrezco, así como algunas fotos de mis trabajos anteriores.
+              Ofrecemos servicios de alta calidad en arreglos y alteraciones de ropa para asegurar que tus prendas se ajusten perfectamente a ti.
             </p>
             <a
               href="#"
               className="bg-black text-gray-50 py-3 px-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 inline-block"
             >
-              Ver más
+             Contactar
             </a>
           </div>
         </div>
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -45,7 +45,7 @@ export const NavBar = () => {
                 <li>
                   <Link
                     href={"/contact"}
-                    className="bg-gradient-to-br from-white to-orange-500 bor text-black px-4 py-2 rounded-md hover:bg-gray-900 transition-colors duration-300"
+                    className="bg-gradient-to-br from-white to-grey-500 bor text-black px-4 py-2 rounded-md hover:bg-gray-900 transition-colors duration-300"
                   >
                     Contacto
                   </Link>

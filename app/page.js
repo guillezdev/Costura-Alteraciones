bdbdfcdc398428e8c3cb7dc9ef5c08c56ba470f4
guillezdev <guillermoprogcc@gmail.com>
2023-06-11 09:36:31 +0200

    last vercion

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
+import Abaut from "@/components/Abaut";
+import Contact from "@/components/Contact";
 import Header from "@/components/Header";
 import Services from "@/components/Services";
+import Testimonials from "@/components/Testimonials";
 
 
 export default function Home() {
@@ -7,6 +10,9 @@ export default function Home() {
     <main>
       <Header />
       <Services />
+      <Testimonials />
+      <Abaut />
+      <Contact />
     </main>
   );
 }

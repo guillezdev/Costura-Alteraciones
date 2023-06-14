c0206a2ea071abf4a3a57f4ad8b44f318bfca711
guillezdev <guillermoprogcc@gmail.com>
2023-06-14 07:43:23 +0200

    add text in the testimonials

--- components/TestimonialCard .jsx
+++ components/TestimonialCard .jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { FaStar } from 'react-icons/fa';
-import 'tailwindcss/tailwind.css';
+import React from "react";
+import { FaStar } from "react-icons/fa";
+import "tailwindcss/tailwind.css";
 
-const TestimonialCard = () => {
+const TestimonialCard = ({name ,description}) => {
   return (
-    <div className="flex flex-col lg:flex-row items-center lg:items-start bg-white rounded-lg shadow-md p-6 lg:p-8">
+    <div className="flex flex-col lg:flex-row items-center lg:items-start bg-stone-800 rounded-lg shadow-md p-6 lg:p-8">
       <div className="w-full lg:w-1/3 mb-4 lg:mb-0 lg:mr-8">
         <img
           src="https://via.placeholder.com/300x200"
@@ -13,16 +13,8 @@ const TestimonialCard = () => {
         />
       </div>
       <div className="w-full lg:w-2/3">
-        <h3 className="text-2xl font-bold mb-2">Título del testimonio</h3>
-        <p className="text-gray-700 mb-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod
-          mauris eget nisi laoreet pharetra. Nullam porta massa eu nisi
-          scelerisque, nec suscipit mauris molestie. Sed sollicitudin, ipsum in
-          ultricies hendrerit, quam turpis hendrerit ligula, in consequat velit
-          neque id ex. Ut tristique commodo metus, ac gravida ex dictum sit
-          amet. Integer elementum, tellus a auctor mattis, dolor ligula
-          imperdiet turpis, vel fringilla dui eros vitae libero.
-        </p>
+        <h3 className="text-2xl text-gray-50 font-bold mb-2">{name}</h3>
+        <p className="text-stone-300 mb-4">{description}</p>
         <div className="flex items-center">
           <div className="flex items-center">
             <FaStar className="text-yellow-400 mr-1" />
--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -6,15 +6,29 @@ import TestimonialCard from "./TestimonialCard ";
 export default function SlideAsAnything() {
   return (
     <section className="h-screen bg-black">
+      <div className="w-full flex justify-center flex-col items-center p-5">
+        <h1 className="text-center text-white text-4xl pt-4">
+          Que piensan nestros clientes?
+        </h1>
+      </div>
       <Carousel>
-        <div className="flex items-center justify-center gap-2 bg-white text-black">
-          <TestimonialCard />
+        <div className="flex h-full items-center justify-center bg-black">
+          <TestimonialCard
+            name="Luisa"
+            description="Acudí a Loraine para que me ayudara a arreglar un vestido que tenía desde hace tiempo y que me encantaba, pero que ya no me quedaba bien. La atención que recibí fue excelente, y ella se tomó el tiempo para entender exactamente lo que quería y lograr el ajuste perfecto. Sin duda, recomendaría sus servicios de costura a cualquiera que necesite ayuda para renovar su ropa."
+          />
         </div>
         <div className="flex h-full items-center justify-center bg-black">
-          <TestimonialCard />
+          <TestimonialCard
+            name="Jorge"
+            description="Loraine es una costurera muy talentosa y dedicada. La conocí cuando necesitaba ayuda para arreglar mi traje de gala para una boda. Desde el primer momento, me hizo sentir cómodo y confiado en su habilidad. El resultado final fue impresionante, y recibí muchos cumplidos sobre lo bien que me veía. Siempre acudiré a ella para cualquier necesidad de costura que tenga en el futuro."
+          />
         </div>
         <div className="flex h-full items-center justify-center bg-black">
-        <TestimonialCard />
+          <TestimonialCard
+            name="Cecilia"
+            description="No puedo expresar lo feliz que estoy con el trabajo que hizo Loraine en mi vestido de graduación. Fue un proceso emocionante, pero también estresante, ya que quería que todo fuera perfecto. Loraine no solo ajustó el vestido a mi cuerpo de manera increíble, sino que también me brindó tranquilidad y confianza durante todo el proceso. Definitivamente volveré a acudir a sus servicios de costura en el futuro."
+          />
         </div>
       </Carousel>
     </section>

11709f0ddcedfab5c4c9aa8099daace795f18ec3
guillezdev <guillermoprogcc@gmail.com>
2023-06-15 04:30:56 +0200

    change styles

--- components/Abaut.jsx
+++ components/Abaut.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const About = () => {
   return (
     <section id="about" className="flex flex-col md:flex-row items-center md:justify-center md:px-20 py-16 md:py-24 bg-stone-900">
-      <div className="md:w-1/2 md:pr-8 mb-8 md:mb-0">
+      <div className="md:w-1/2 md:pr-8 mb-8 md:mb-0 px-2">
         <h2 className="text-4xl  text-stone-50 font-bold mb-4">Sobre mí</h2>
         <p className="text-stone-300 leading-relaxed">
           Soy una costurera con más de <span className='bg-stone-700 rounded-md p-1'>+20</span> años de experiencia en el sector. Me
@@ -16,7 +16,7 @@ const About = () => {
         <div className='flex gap-3 text-stone-300'><p>a</p> <p>b</p> <p>c</p></div>
       </div>
       <div className="md:w-1/2">
-        <img src="https://www.ohchr.org/sites/default/files/styles/hero_5_image_desktop/public/2022-11/women-rights-main-image.jpg?itok=RRGl2PFb" alt="Sobre mí" className="rounded-lg shadow-xl shadow-black" />
+        <img src="https://www.ohchr.org/sites/default/files/styles/hero_5_image_desktop/public/2022-11/women-rights-main-image.jpg?itok=RRGl2PFb" alt="Sobre mí" className="rounded-lg shadow-xl shadow-black px-2" />
       </div>
       
     </section>

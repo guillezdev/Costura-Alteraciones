fdddab7b8d59f0ebbfb978afe5899f0a0467b64f
guillezdev <guillermoprogcc@gmail.com>
2023-06-18 02:00:30 +0200

    changed contact number

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -7,9 +7,9 @@ const ContactSection = () => {
       <div className="max-w-screen-md mx-auto px-4">
         <h2 className="text-4xl font-bold mb-8">Contacto</h2>
         <div className="flex flex-col md:flex-row gap-2">
-          <a href="tel:+1(786)823-9937" className="flex items-center mb-4 md:mb-0 md:mr-4">
+          <a href="tel:+1(786)813-9937" className="flex items-center mb-4 md:mb-0 md:mr-4">
             <FaPhoneAlt size={24} className="text-gray-50 mr-2" />
-            <span>{`+1 (786) 823-9937`}</span>
+            <span>{`+1 (786) 813-9937`}</span>
           </a>
           <div className="flex items-center mb-4 md:mb-0 md:mr-4">
             <MdLocationOn size={24} className="text-gray-50 mr-2" />
